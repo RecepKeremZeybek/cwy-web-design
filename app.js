e6c9435d8a7b6b8467754c6f2413ebe8726a2321
KeremZeybek <recepkeremzeybek@gmail.com>
2023-05-19 23:06:17 +0300

    header finished

--- app.js
+++ app.js
@@ -2,3 +2,67 @@
 
 //? TW -3 Javascript
 
+//* Soru -8
+
+//  const x = 6 % 4;
+//  const y = x ? "One" : "Two";
+//  console.log(y)
+
+
+//* Soru -11
+
+// Code A
+// for (let number = 10; number >= 1; number--) {
+//     console.log(number);
+//    }
+
+   //!!!!!! Code B
+
+//    let number = 10;
+//    while (number >= 1) {
+//     console.log(number);
+//     number++;
+//    }
+   
+
+//*----------------------------
+
+// let sayi =5;
+
+// {
+//     let sayi=7
+//     console.log("local sayi deişkeni",sayi)
+// }
+
+// console.log("global sayi değişkeni",sayi)
+
+//* Reverse String
+
+// function reverse1(str){
+//     return str.split('').reverse().join('')
+// }
+// console.log(reverse1("Bugün günlerden Cuma"))
+// console.log(reverse1("Clarusway"))
+
+
+//* Reverse Words
+
+
+// function reverse2(str){
+//     return str.split(' ').reverse().join(' ')
+// }    
+
+// console.log(reverse2("Bugün hava yağmurlu"))
+// console.log(reverse2("Clarusway"))
+
+// //? reverse string döngü ile  string olmadan
+
+// let reverse= function(str){
+    
+//  for(let i = str.length -1   ; i>=0; i--){
+//    console.log(str[i])
+//   }
+
+
+// }
+// console.log(reverse("Bugün hava çok güzel"))
